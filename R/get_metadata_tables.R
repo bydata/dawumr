ff887af302cc7f8ba610a6ac52cf560f9f09c17c
bydata <ansgar.wolsing@gmail.com>
2023-10-06 21:58:19 +0200

    bug fix wrong obj

--- R/get_metadata_tables.R
+++ R/get_metadata_tables.R
@@ -13,8 +13,8 @@
 #' @examples \dontrun{get_metadata_tables(x)}
 get_metadata_tables <- function(x) {
   table_names <- c("Parliaments", "Institutes", "Taskers", "Methods", "Parties")
-  db <- purrr::map(table_names, ~dplyr::bind_rows(obj[[.x]], .id = "ID") |>
-                     dplyr::mutate(ID = as.numeric(ID)) |>
+  db <- purrr::map(table_names, ~dplyr::bind_rows(x[[.x]], .id = "ID") |>
+                     dplyr::mutate(ID = as.integer(ID)) |>
                      dplyr::arrange(ID))
   db <- purrr::set_names(db, table_names)
   return(db)
