b72049abd0ce6c48f5c97f4cd0b602e500efc537
bydata <ansgar.wolsing@gmail.com>
2023-10-06 21:54:03 +0200

    rename get_dawum_data to dawum

--- R/dawum.R
+++ R/dawum.R
@@ -3,14 +3,16 @@
 #' @description This function retrieves data from DAWUM API, extracts relevant data
 #'     from the API response and returns a list object with all metadata and
 #'     survey results.
+#' @param denormalize Logical. Should the names of parties, institutes etc. be
+#'    added to the results data frame? Default: TRUE
 #' @param verbose Logical. Should messages be printed? Default: TRUE
 #'
 #' @return A list object which contains all metadata and survey results
 #' @importFrom purrr map_dfr
 #' @export
 #'
-#' @examples \dontrun{get_dawum_data()}
-get_dawum_data <- function(verbose = TRUE) {
+#' @examples \dontrun{dawum()}
+dawum <- function(denormalize = FALSE, verbose = TRUE) {
 
   # Retrieve data from API
   if (verbose) message("Retrieving data from DAWUM API.")
@@ -25,6 +27,13 @@ get_dawum_data <- function(verbose = TRUE) {
   if (verbose) message("Extracting survey results.")
   db$Results <- purrr::map_dfr(obj$Surveys, generate_result, .id = "ID")
 
+  # Denormalize tables - this will add names for parties, parliaments etc. to the
+  # data frame // default: FALSE
+  if (denormalize) {
+    db <- denormalize(db)
+  }
+
+  if (verbose) message(sprintf("Last update: %s", db$last_update))
   if (verbose) message("DONE.")
   return(db)
 }
