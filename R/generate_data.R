9cb1ee72f49e06a2366b0587da9ed1e0d87d9fb1
bydata <ansgar.wolsing@gmail.com>
2023-10-10 11:38:22 +0200

    remove dependency on dplyr and purrr from generate_data.R

--- R/generate_data.R
+++ R/generate_data.R
@@ -8,15 +8,17 @@
 #' @examples \dontrun{generate_result_metadata(x)}
 generate_result_metadata <- function(x) {
   variable_names <- c("Date", "Surveyed_Persons", "Parliament_ID", "Institute_ID",
-                      "Tasker_ID", "Method_ID" )
-  metadata <- purrr::map(variable_names, ~purrr::pluck(x, .x)) |>
-    purrr::set_names(variable_names) |> dplyr::bind_cols()
+                      "Tasker_ID", "Method_ID")
+
+  metadata <- lapply(variable_names, function(var_name) x[[var_name]])
+  names(metadata) <- variable_names
+  metadata <- as.data.frame(metadata)
+
   metadata$Survey_Period_Start <- x$Survey_Period$Date_Start
   metadata$Survey_Period_End <- x$Survey_Period$Date_End
-  # Convert ID variables to integer
-  metadata <- metadata |>
-    dplyr::mutate(dplyr::across(dplyr::ends_with("ID"),
-                                as.integer))
+
+  id_columns <- grep("_ID$", names(metadata))
+  metadata[, id_columns] <- lapply(metadata[, id_columns], as.integer)
 
   return(metadata)
 }
@@ -29,14 +31,12 @@ generate_result_metadata <- function(x) {
 #' @param x A Survey list object from dawum API data
 #'
 #' @return A data frame with the results from a survey
-#' @importFrom purrr reduce
 #' @export
 #'
 #' @examples \dontrun{generate_result_dataframe(x)}
 generate_result_dataframe <- function(x) {
   result <- data.frame(
-    # TODO: Which of these steps are required?
-    Share = cbind(x$Results) |> purrr::reduce(c) |> unlist() |> as.numeric()
+    Share = cbind(x$Results) |> unlist() |> as.numeric()
   )
   result$Party_ID <- as.numeric(rownames(result))
   rownames(result) <- NULL
