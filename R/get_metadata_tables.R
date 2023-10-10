d3a3b3b667c43a2c098767c618a7e1ceeab4a52f
bydata <ansgar.wolsing@gmail.com>
2023-10-10 12:10:44 +0200

    eliminate dependency on purrr::map, dplyr::mutate, and dplyr::arrange from get_metadata_tables

--- R/get_metadata_tables.R
+++ R/get_metadata_tables.R
@@ -4,18 +4,18 @@
 #' @param x A list object containing the fetched API response
 #'
 #' @return A list object
-#' @importFrom purrr map
-#' @importFrom dplyr mutate
-#' @importFrom dplyr arrange
 #' @importFrom dplyr bind_rows
 #' @export
 #'
 #' @examples \dontrun{get_metadata_tables(x)}
 get_metadata_tables <- function(x) {
   table_names <- c("Parliaments", "Institutes", "Taskers", "Methods", "Parties")
-  db <- purrr::map(table_names, ~dplyr::bind_rows(x[[.x]], .id = "ID") |>
-                     dplyr::mutate(ID = as.integer(ID)) |>
-                     dplyr::arrange(ID))
-  db <- purrr::set_names(db, table_names)
+  fun <- function(table_name) {
+    x[[table_name]] <- dplyr::bind_rows(x[[table_name]], .id = "ID")
+    x[[table_name]]$ID <- as.integer(x[[table_name]]$ID)
+    x[[table_name]] <-  x[[table_name]][order(x[[table_name]]$ID), ]
+  }
+  db <- lapply(table_names, fun)
+  names(db) <- table_names
   return(db)
 }
